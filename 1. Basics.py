3be8ed7466c34ecbd259e60c1d3723c8b11fdb08
rawkush <ankushrawat950@gmail.com>
2018-06-08 18:18:16 +0530

    from hello world to taking input

--- 1. Basics.py
+++ 1. Basics.py
@@ -1,1 +1,14 @@
 print("Hello world")
+
+# this is comment line '#' is used for single commenting a line
+
+
+#part 2 lets see how to take data from user
+
+
+data= input("user enter your data: ")
+
+print("the data entered by user is ", data)
+
+
+
