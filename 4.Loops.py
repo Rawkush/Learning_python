e588cb82f51dd5d0e704fdc6eae0e4e2bc448cc4
rawkush <ankushrawat950@gmail.com>
2018-06-14 18:03:53 +0530

    for loop

--- 4.Loops.py
+++ 4.Loops.py
@@ -7,3 +7,16 @@ while (v<=10):
     # v++ is invalid as ++ is not any operator in python
     v=v+1
 print("loop ended")
+
+
+#######################################################
+
+#for loop and using >>in<<
+
+for symbol in "internshala":
+    print(symbol)
+
+
+
+# s0 for loop start from index 0 of the element and work till the end
+#also auto incremented
