1486ebabf93742d1c9774ddc7261842a8f3fe0dd
rawkush <ankushrawat950@gmail.com>
2018-06-17 14:25:12 +0530

    globar varible vs local

--- 5. Function.py
+++ 5. Function.py
@@ -5,3 +5,29 @@ def function_name():
     print("created the function and inside the function cui=rrently")
     return
     
+#global variable
+
+age=17
+
+def newFunction():
+    age=12 ## this age is local variable
+    print(age)
+    return
+
+newFunction()
+
+print(age)
+
+#to chanege the gl,obal variable
+
+def changing():
+    
+    global qge
+    age=12 ## this age is local variable
+    print(age)
+    return
+
+
+changing()
+print("global changed",age)
+    
