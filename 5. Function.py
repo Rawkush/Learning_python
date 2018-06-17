6f1146291886e44c0165c3aeb583b4e31a0c7ca5
rawkush <ankushrawat950@gmail.com>
2018-06-17 14:02:08 +0530

    return

--- 5. Function.py
+++ 5. Function.py
@@ -2,5 +2,6 @@
 
 def function_name():
     "explainatory string ius wriiten here, it is smilar to comment" # it is optional
-    
+    print("created the function and inside the function cui=rrently")
+    return
     
