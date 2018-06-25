0956c47df271edbb5d9357c07e8f08da57e086c7
rawkush <ankushrawat950@gmail.com>
2018-06-25 12:11:00 +0530

    creating a class and method in it

---
+++ 8. Classes.py
@@ -0,0 +1,15 @@
+#syntax of class
+class className:
+    name="ankush"
+    gender="male"
+    def ShowInfo(self): #method in class
+        print('NAme:',self.name)
+        print('gender',self.gender)
+
+
+
+#outside of class now
+
+p1=className()
+p1.ShowInfo()
+    
