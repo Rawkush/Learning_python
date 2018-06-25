e037e42d844403f78ec54875e69e54581495856b
rawkush <ankushrawat950@gmail.com>
2018-06-25 12:17:35 +0530

    method with arguments

--- 8. Classes.py
+++ 8. Classes.py
@@ -12,4 +12,22 @@ class className:
 
 p1=className()
 p1.ShowInfo()
-    
+
+
+#creating a class which initialisees the data members from user input
+
+
+class person:
+    def __init__(self,a='ravi',b='male'):
+                 self.name=a
+                 self.gender=b
+    def ShowInfo(self):
+                 
+                 print('NAme:',self.name)
+                 print('gender',self.gender)
+
+
+
+
+p2=person('ank','male')
+p2.ShowInfo()
