fc14032d5a0e4372090798e53bcaccfcafdb93ac
rawkush <ankushrawat950@gmail.com>
2018-08-19 16:28:28 +0530

    2 nd part of assignment completed

--- Nptel_Assignments/first.py
+++ Nptel_Assignments/first.py
@@ -6,3 +6,23 @@ def intreverse(n):
 
     n=int(temp)
     return n
+
+#second function
+
+def matched(s):
+    x=0
+    y=0
+    for var in s:
+        if var=="(":
+            x=x+1           
+            
+        if var==")":
+            y=y+1
+            if y>x:
+                return False
+
+    if x==y:
+        return True
+    else:
+        return False
+
