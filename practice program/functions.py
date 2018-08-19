36a4cd1a0a09b2abc8fb57a373ea635741f8f88a
rawkush <ankushrawat950@gmail.com>
2018-08-19 16:00:01 +0530

    removed invalid synatx assignment

--- practice program/functions.py
+++ practice program/functions.py
@@ -44,7 +44,6 @@ y = x[0:50]                    # Statement 2
 z = y                          # Statement 3
 w = x                          # Statement 4
 x[1] = x[1] + 'd'              # Statement 5
-x[1][1] = 'y'                  # Statement 6
 y[2] = 4                       # Statement 7
 z[0] = 0                       # Statement 8
 w[4][0] = 1000                 # Statement 9
