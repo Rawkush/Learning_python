21dbb1984cc2ef5bbc7f76c55b2c6a0265384412
rawkush <ankushrawat950@gmail.com>
2018-08-19 16:16:53 +0530

    1 qestion solution added

---
+++ Nptel_Assignments/first.py
@@ -0,0 +1,8 @@
+def intreverse(n):
+    st=str(n)
+    temp=""
+    for var in st:
+        temp=var+temp
+
+    n=int(temp)
+    return n
--- practice program/functions.py
+++ practice program/functions.py
@@ -73,6 +73,13 @@ for i in range(0,len(startmsg)):
 print(endmsg)
 
 
+def mystery(l):
+    l = l + l
+    return()
+
+mylist = [31,24,75]
+mystery(mylist)
+print(mylist)
 
 
 
