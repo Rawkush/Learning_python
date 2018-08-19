0f5cf15071e59d07142e593e312f9327f8c622b4
rawkush <ankushrawat950@gmail.com>
2018-08-19 16:52:52 +0530

    added code for 3rd part

--- Nptel_Assignments/first.py
+++ Nptel_Assignments/first.py
@@ -26,3 +26,54 @@ def matched(s):
     else:
         return False
 
+# 3rd function
+
+def sumprimes(l):
+    s=0
+    for var in l:
+        if var>0:
+            if prime(var):
+                s=s+var
+    return s
+
+def prime(num):
+    print(num)
+
+    for i in range(2, int(num/2)+1):
+        print(num%i)
+
+        if num%i== 0:
+            return False;
+            break
+    return True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
