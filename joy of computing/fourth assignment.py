e8c45edfbff5850b592ec0943dffed0dcf058fe6
rawkush <ankushrawat950@gmail.com>
2018-09-02 19:12:38 +0530

    second question solution

---
+++ joy of computing/fourth assignment.py
@@ -0,0 +1,28 @@
+a,b=input().split()
+a=int(a)
+b=int(b)
+k=1
+c=1
+for i in range(a):
+  for j in range(b):
+         
+    print(k,end="")
+    if c<b:  
+      print(" ",end="")
+    c+=1
+    k+=1
+  c=1  
+  print()
+
+###NEXT
+
+num=int(input())
+factorial=1
+if num == 0:
+   print("1")
+else:
+  
+  for i in range(1,num + 1):
+    
+    factorial = factorial*i
+  print(factorial)
