e8bea91c2e6c48a9ca532740a7922b8883c7e838
rawkush <ankushrawat950@gmail.com>
2018-09-06 21:46:00 +0530

    3rd assignment first question done

--- Nptel_Assignments/second.py
+++ Nptel_Assignments/second.py
@@ -15,3 +15,21 @@ def transpose(m):
   return(new)
 
 #next one
+
+
+def descending(list):
+    flag=0
+    if(len(list)is 0):
+        return True
+    else:
+        for i in range(0,len(list)):
+            if(i>0 and list[i]<=list[i-1]):
+                flag=flag+1
+            
+    if(flag is (len(list)-1)):
+        return True
+    else:
+        return False
+
+
+#next one
